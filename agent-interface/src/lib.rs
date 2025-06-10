1e07507c8514ca997b447fdf8a8690af9915488b
Atsuyo64 <victorlasserre78@gmail.com>
2025-06-10 14:53:25 +0000

    feat: more generic game trait + test

--- agent-interface/src/lib.rs
+++ agent-interface/src/lib.rs
@@ -1,22 +1,53 @@
-pub trait Agent<State,Action> {
+pub trait Agent<State, Action> {
     fn init() -> Self;
 
-    //State : serializable ? string ?
+    //State : Serializable ? string ?
     fn select_action(&mut self, state: State) -> Option<Action>;
 }
 
-pub trait Game<State,Action> {
-    fn init() -> Self;
-    fn apply_action(&mut self, action : &Action) -> Result<(),()>;
+pub trait Game<State, Action> {
+    fn init(&mut self);
+    fn apply_action(&mut self, action: &Action) -> Result<(), ()>; //non mutable ?, -> bool ?
     fn is_finished(&self) -> bool;
+    fn number_of_players(&self) -> u32; //self necessary for vtable
+    fn is_deterministic(&self) -> bool;
+
+    //Supposed true for now
+    //fn is_perfect_information(&self) -> bool;  //*has* ?
+
+    //Supposed true for now
+    // fn is_sequential(&self) -> bool;
 }
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
+
+    struct DummyGame {}
+
+    impl Game<(), ()> for DummyGame {
+        fn init(&mut self) {}
+
+        fn apply_action(&mut self, _action: &()) -> Result<(), ()> {
+            Ok(())
+        }
+
+        fn is_finished(&self) -> bool {
+            false
+        }
+
+        fn number_of_players(&self) -> u32 {
+            0
+        }
+
+        fn is_deterministic(&self) -> bool {
+            true
+        }
+    }
 
     #[test]
     fn it_works() {
-        assert!(true);
+        let game: Box<dyn Game<(),()>> = Box::new(DummyGame{});
+        assert!(game.is_deterministic());
     }
 }
