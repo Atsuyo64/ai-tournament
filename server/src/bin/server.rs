c363acafccea5f497cf140d6c604f1d4188e3528
Atsuyo64 <132666940+Atsuyo64@users.noreply.github.com>
2025-06-18 17:04:08 +0200

    feat: tests in server socket creation

--- server/src/bin/server.rs
+++ server/src/bin/server.rs
@@ -1,8 +1,9 @@
 use anyhow::{self, Context};
 
 fn main() -> anyhow::Result<()> {
-    let address = "127.0.0.1:0";
+    let address = "[::]:0";
     let server = std::net::TcpListener::bind(address).context("creating server socket")?;
-
+    println!("Address: {}",server.local_addr().unwrap());
+    //FIXME: check if ipv6 is available !
     Ok(())
 }
