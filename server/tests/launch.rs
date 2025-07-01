d5d068d8c40ae2be8cb4de5578074a5953cf52bb
Atsuyo64 <132666940+Atsuyo64@users.noreply.github.com>
2025-07-01 14:49:48 +0200

    feat!: log -> tracing and env_logger -> tracing-subscriber

--- server/tests/launch.rs
+++ server/tests/launch.rs
@@ -1,31 +1,27 @@
 use crate::games::{DummyFactory, RPSWrapper};
 use ::server::server::Evaluator;
 use server::server;
-use std::{io::Write, str::FromStr};
+use std::str::FromStr;
+use tracing::Level;
 
 mod games;
 
 fn init_logger() {
-    let env = env_logger::Env::default()
-        .filter_or("RUST_LOG_LEVEL", "debug")
-        .write_style_or("RUST_LOG_STYLE", "auto");
+    let format = tracing_subscriber::fmt::format()
+        .without_time()
+        .with_ansi(true)
+        .with_level(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false)
+    ;
 
-    let _ = env_logger::Builder::from_env(env)
-        .is_test(false)
-        .format_timestamp(None)
-        .format(|buf, record| {
-            let style = buf.default_level_style(record.level());
-            let tid= std::thread::current().id();
-            writeln!(
-                buf,
-                "[{style}{:<5}{style:#} {:?} {}] {}",
-                record.level(),
-                tid,
-                record.module_path().unwrap_or(""),
-                record.args()
-            )
-        })
-        .try_init(); //NOTE: might change is_test to false
+    let _ = tracing_subscriber::fmt()
+        .event_format(format)
+        .with_max_level(Level::INFO)
+        .try_init();
 }
 
 #[test]
@@ -42,14 +38,16 @@ fn launch_dummy() {
 fn launch_rock_paper_scissors() {
     init_logger();
 
-    let params  = server::SystemParams::new(server::MaxMemory::Auto, server::AvailableCPUs::Auto);
+    let params = server::SystemParams::new(server::MaxMemory::Auto, server::AvailableCPUs::Auto);
     let evaluator = Evaluator::new(RPSWrapper::default(), params);
-    let path = std::env::current_dir().unwrap().join("tests/rock_paper_scissors_agents");
+    let path = std::env::current_dir()
+        .unwrap()
+        .join("tests/rock_paper_scissors_agents");
     let _ = evaluator.evaluate(path.as_path()).unwrap();
 }
 
 #[test]
-fn test_from_str() -> Result<(),String> {
+fn test_from_str() -> Result<(), String> {
     let _state = crate::games::PlayerState::from_str("0\n")?;
     Ok(())
 }
