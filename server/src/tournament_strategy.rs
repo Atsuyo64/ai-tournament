a1a19e6c947f4b48ecb59752e22d00256da8a7c8
Atsuyo64 <132666940+Atsuyo64@users.noreply.github.com>
2025-07-07 13:41:48 +0200

    feat: round robin tournament and singleplayer tournament

--- server/src/tournament_strategy.rs
+++ server/src/tournament_strategy.rs
@@ -2,7 +2,8 @@ use std::sync::Arc;
 
 use crate::{agent::Agent, tournament::Scores};
 
-pub trait TournamentStrategy { //CHECK: template number of player per match ? (probably a bad idea)
+pub trait TournamentStrategy {
+    //TODO: if deffering responsability to choose tournament to caller, add `init` parameter-only constructor (actually not here: parameters are strategy-specific)
     fn advance_round(&mut self, scores: &Scores);
     fn get_pending_tuples(&mut self) -> Vec<Vec<Arc<Agent>>>;
     fn is_complete(&self) -> bool;
@@ -64,5 +65,70 @@ impl TournamentStrategy for SwissStrategy {
     fn players_per_match(&self) -> usize {
         2
     }
-    
+}
+
+pub struct RoundRobinStrategy {
+    pending: Vec<Vec<Arc<Agent>>>,
+}
+
+impl RoundRobinStrategy {
+    /// symmetric means (A VS B) should give the same result as (B VS A)
+    pub fn new(agents: Vec<Arc<Agent>>, symmetric: bool) -> Self {
+        let n = agents.len();
+        let mut pending = vec![];
+        for i in 0..n {
+            for j in i..n {
+                pending.push(vec![agents[i].clone(), agents[j].clone()]);
+                if !symmetric {
+                    pending.push(vec![agents[j].clone(), agents[i].clone()]);
+                }
+            }
+        }
+        Self { pending }
+    }
+}
+
+impl TournamentStrategy for RoundRobinStrategy {
+    fn advance_round(&mut self, _scores: &Scores) {}
+
+    fn get_pending_tuples(&mut self) -> Vec<Vec<Arc<Agent>>> {
+        std::mem::take(&mut self.pending)
+    }
+
+    fn is_complete(&self) -> bool {
+        self.pending.is_empty()
+    }
+    fn players_per_match(&self) -> usize {
+        2
+    }
+}
+
+pub struct SingleplayerTournamentStrategy {
+    pending: Vec<Vec<Arc<Agent>>>,
+}
+
+impl SingleplayerTournamentStrategy {
+    /// game_per_agent 
+    pub fn new(agents: Vec<Arc<Agent>>, game_per_agent : usize) -> Self {
+        let mut pending = vec![];
+        for agent in agents {
+            pending.append(&mut vec![vec![agent];game_per_agent]);
+        }
+        Self { pending }
+    }
+}
+
+impl TournamentStrategy for SingleplayerTournamentStrategy {
+    fn advance_round(&mut self, _scores: &Scores) {}
+
+    fn get_pending_tuples(&mut self) -> Vec<Vec<Arc<Agent>>> {
+        std::mem::take(&mut self.pending)
+    }
+
+    fn is_complete(&self) -> bool {
+        self.pending.is_empty()
+    }
+    fn players_per_match(&self) -> usize {
+        1
+    }
 }
